0888d78a0515c8147c1be050ae98d1871c4823e6
Juanma1313 <j.heras13@gmail.com>
2024-03-10 14:15:13 +0100

    Add view for instructions

--- dys/views.py
+++ dys/views.py
@@ -19,11 +19,13 @@ class ThingDetail(View):
             thing = get_object_or_404(queryset, slug=slug)
             print(f"thing={thing.title}")
             instructions = thing.instructions.order_by("title")
-            components = thing.components.filter(status=1, parent = None).order_by("title")
+            components = thing.components.filter(status=1).order_by("title")
             liked = False
             if thing.likes.filter(id=self.request.user.id).exists():
                 liked = True
-            print(thing.featured_image.url )
+            print(f"Components: {components}")
+            for step in instructions:
+                print(f"Step: {step.title}, Instructions:{step.instructions}")
             return render(
                 request,
                 "thing_detail.html",
