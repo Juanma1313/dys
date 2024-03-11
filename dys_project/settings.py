aa57a962cb0153677937a2b72874ef40edcee62d
Juanma1313 <j.heras13@gmail.com>
2024-03-11 20:18:55 +0100

    Add email credentials

--- dys_project/settings.py
+++ dys_project/settings.py
@@ -20,6 +20,9 @@ DEBUG = False
 if os.path.isfile("env.py"):
     import env
 
+DEBUG = os.environ.get('DEVELOPMENT')
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -165,8 +168,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
