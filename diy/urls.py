55181c8141a6f55a5e6556bff022f8c303dc333f
Juanma1313 <j.heras13@gmail.com>
2024-08-08 17:03:08 +0200

    Add Comments functionality to templates, urls, and js files

--- diy/urls.py
+++ diy/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('<slug:slug>/', views.ThingDetail.as_view(), name='thing_detail'),
     path('like/<slug:slug>', views.ThingLike.as_view(), name='thing_like'),
     path('user/<str:form>', views.UserProfile.as_view(), name='user'),
-    ]
+    path('<slug:slug>/edit_comment/<int:comment_id>',
+         views.comment_edit, name='comment_edit'),
+    path('<slug:slug>/delete_comment/<int:comment_id>',
+         views.comment_delete, name='comment_delete'),
+]
